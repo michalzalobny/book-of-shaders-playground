755c8bce3ae710ef6489532e2e67b361eb28641a
michalzalobny <michal.zalobny@lepolishbureau.com>
2021-12-30 06:55:12 -0800

    Add author info

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import { LinkHandler } from 'components/LinkHandler/LinkHandler';
+
 import styles from './Layout.module.scss';
 
 interface Props {
@@ -14,6 +16,13 @@ export const Layout = (props: Props) => {
   return (
     <>
       <div className={clsx(styles.readyWrapper, isReady && styles.readyWrapperReady)}></div>
+      <div className={styles.authorWrapper}>
+        <span className={styles.contactText}>Shaders Playground by</span>
+        <span className={styles.contactSpacer} />
+        <LinkHandler isExternal elHref="https://www.linkedin.com/in/michal-zalobny-1a8257204/">
+          <span className={clsx(styles.contactText, styles.contactTextBold)}>Michal Zalobny</span>
+        </LinkHandler>
+      </div>
       {children}
     </>
   );
