014dccb1028a4a96ac5525407d8a4379cb36a082
michalzalobny <michal.zalobny@lepolishbureau.com>
2021-12-30 04:42:29 -0800

    Update shared styles

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { Head } from 'seo/Head/Head';
 
 import styles from './IndexPage.module.scss';
+import sharedStyles from 'utils/sharedStyles.module.scss';
 
 export default function IndexPage() {
   return (
     <>
       <Head />
-      <div className={styles.wrapper}>test</div>
+      <div className={styles.wrapper}>
+        <div className={sharedStyles.blobBtn}>dasd</div>
+      </div>
     </>
   );
 }
