b09a11e163ef13d89f3c80952e71b9ab294bd4ce
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-02 05:20:41 -0800

    Update second shader

--- src/containers/Shaders/2/fragment.glsl
+++ src/containers/Shaders/2/fragment.glsl
@@ -6,17 +6,8 @@ uniform vec2 uMouse;
 varying vec2 vUv;
 
 
-float getStartXValue(){
-    return (uCanvasRes.x - uPlaneRes.x) * 0.5;
-}
-
-float getStartYValue(){
-    return (uCanvasRes.y - uPlaneRes.y) * 0.5;
-}
-
-
 void main()
 {
     vec2 st = uMouse.xy; 
-    gl_FragColor = vec4((st.x - getStartXValue()) / uPlaneRes.x , (st.y - getStartYValue()) / uPlaneRes.y, 0.0, 1.0);
+    gl_FragColor = vec4(st.x/uCanvasRes.x ,st.y/uCanvasRes.y, 0.0, 1.0);
 }
