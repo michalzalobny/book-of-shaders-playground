d57359f61734ec6f4eeaef89a5403c9250ce5a88
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-03 02:56:57 -0800

    Update shader

--- src/containers/Shaders/4/fragment.glsl
+++ src/containers/Shaders/4/fragment.glsl
@@ -6,17 +6,15 @@ uniform float uPixelRatio;
 
 varying vec2 vUv;
 
-//mix() used for linear interpolation from value a to b using the function : a(1-x) + xb
 
 float plot(vec2 st) {    
-    return smoothstep(0.02, 0.0, abs(st.y - st.x)); //Used to turn elements on or off => goes from 0 to 1 smoothly in the range of the first and second argument
+    return smoothstep(0.02, 0.0, abs(st.x-st.y)); //Used to turn elements on or off => goes from 0 to 1 smoothly in the range of the first and second argument
 }
 
 void main()
 {
     vec2 st = vUv;
-    st.x -= 0.5; 
-    float fork = smoothstep(0.6, 0.8 ,st.y);
-    vec3 col = vec3(smoothstep(0.03 * (1.-fork * 0.8), 0., abs(abs(st.x)-0.05 * fork)));
+    float pct = plot(st);
+    vec3 col = (1. - pct ) * vec3(st.x) + vec3(0., pct, 0.);
     gl_FragColor = vec4(col, 1.0);
 }
