48e2a79e10baa10c8bee535ca0cac350f5133898
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-09 16:27:30 +0100

    Fix mouse shader

--- src/containers/Shaders/19/fragment.glsl
+++ src/containers/Shaders/19/fragment.glsl
@@ -13,33 +13,19 @@ float random(vec2 st)
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
-mat2 rotate2d(float _angle){
-    return mat2(cos(_angle),-sin(_angle),
-                sin(_angle),cos(_angle));
-}
-
 void main()
 {
     vec2 mouseNormalized;
     mouseNormalized.x = (uMouse.x - (uCanvasRes.x - uPlaneRes.x) * 0.5) / uPlaneRes.x;
     mouseNormalized.y = ((uCanvasRes.y -  uMouse.y) - (uCanvasRes.y - uPlaneRes.y) * 0.5) / uPlaneRes.y;
 
-    vec2 st = vUv;
-
-    st -= vec2(0.5);
-
-    st = rotate2d( sin(uTime)*PI ) * st;
-
-    // st += vec2(0.5);
-    
-
     float change = (sin(uTime * 0.6) + 1.) / 2.;
-    float angle = atan(st.x, st.y); //The values of atan(x,y) go from -PI to PI
-    angle /= PI * 2.0 ;
+    float angle = atan(vUv.x - mouseNormalized.x, vUv.y - mouseNormalized.y); //The values of atan(x,y) go from -PI to PI
+    angle /= PI * 2.0 * change;
     angle += 0.5;
     float strength = angle;
 
-    strength = step(0.5,mod(strength * 20. , 1.));
+    strength = step(0.5,mod(strength * 20.* change , 1.));
 
     //similar result:
     //float strength = sin(angle * 100.0);
