13cc976a333e6c320895a7b48795042412c92bb1
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-10 23:47:33 +0100

    Add badges support

--- src/components/ShaderTile/ShaderTile.tsx
+++ src/components/ShaderTile/ShaderTile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 
 import { LinkHandler } from 'components/LinkHandler/LinkHandler';
 import { PreloadImage } from 'components/PreloadImage/PreloadImage';
@@ -9,15 +10,21 @@ interface Props {
   imageSrc: string;
   number: string;
   elHref: string;
+  isPro?: boolean;
+  isMotion?: boolean;
 }
 
 export const ShaderTile = (props: Props) => {
-  const { elHref, imageSrc, number } = props;
+  const { isMotion, isPro, elHref, imageSrc, number } = props;
 
   return (
     <>
       <LinkHandler elHref={elHref}>
         <div className={styles.wrapper}>
+          <div className={styles.badgesWrapper}>
+            {isPro && <div className={styles.badge}>Pro</div>}
+            {isMotion && <div className={clsx(styles.badge, styles.badgePurple)}>Motion</div>}
+          </div>
           <div className={styles.numberWrapper}>
             <span className={styles.number}>{number}</span>
           </div>
