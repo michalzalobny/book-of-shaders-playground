93b36b081b8c9c6e13d21b782ece854b25838e87
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-23 16:58:19 +0100

    Add flow distance shader

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -40,7 +40,7 @@ import img32 from 'assets/tileImages/32.jpg';
 import img33 from 'assets/tileImages/33.jpg';
 import img34 from 'assets/tileImages/34.jpg';
 import img35 from 'assets/tileImages/35.jpg';
-// import img36 from 'assets/tileImages/36.jpg';
+import img36 from 'assets/tileImages/36.jpg';
 // import img37 from 'assets/tileImages/37.jpg';
 
 import styles from './IndexPage.module.scss';
@@ -96,6 +96,7 @@ export default function IndexPage() {
           </div>
         </header>
         <div className={styles.tilesWrapper}>
+          <ShaderTile isMotion elHref="/shaders/36" imageSrc={img36.src} number="36" />
           <ShaderTile isMotion elHref="/shaders/35" imageSrc={img35.src} number="35" />
           <ShaderTile isMotion elHref="/shaders/34" imageSrc={img34.src} number="34" />
           <ShaderTile isMotion elHref="/shaders/33" imageSrc={img33.src} number="33" />
--- src/containers/Shaders/36/fragment.glsl
+++ src/containers/Shaders/36/fragment.glsl
@@ -8,16 +8,57 @@ varying vec2 vUv;
 
 #define PI 3.14159265359
 
-float random(vec2 st)
-{
-    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                        vec2(12.9898,78.233)))
+                * 43758.5453123);
+}
+
+// Value noise by Inigo Quilez - iq/2013
+// https://www.shadertoy.com/view/lsf3WH
+float noise(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+    vec2 u = f*f*(3.0-2.0*f);
+    return mix( mix( random( i + vec2(0.0,0.0) ),
+                    random( i + vec2(1.0,0.0) ), u.x),
+                mix( random( i + vec2(0.0,1.0) ),
+                    random( i + vec2(1.0,1.0) ), u.x), u.y);
+}
+
+mat2 rotate2d(float angle){
+    return mat2(cos(angle),-sin(angle),
+                sin(angle),cos(angle));
 }
 
 void main()
 {
-    float r = (distance( vec2(vUv.x, (vUv.y -0.5) * 6.0 + 0.5), vec2(0.5)));
+    vec2 st = vUv;
+    float color;
 
-    float color =  max(min(0.15 / r, 1.), 0.);
+    vec2 mouseNormalized;
+    mouseNormalized.x = (uMouse.x - (uCanvasRes.x - uPlaneRes.x) * 0.5) / uPlaneRes.x;
+    mouseNormalized.y = ((uCanvasRes.y -  uMouse.y) - (uCanvasRes.y - uPlaneRes.y) * 0.5) / uPlaneRes.y;
+
+    // Cell positions
+    vec2 point[5];
+    point[0] = vec2(0.83,0.55);
+    point[1] = vec2(0.60,0.07);
+    point[2] = vec2(0.28,0.64);
+    point[3] =  vec2(0.31,0.26);
+    point[4] = mouseNormalized;
+
+    float m_dist = 1.;  // minimum distance
+
+    // Iterate through the points positions
+    for (int i = 0; i < 5; i++) {
+        float dist = distance(st, point[i]);
+        // Keep the closer distance
+        m_dist = min(m_dist, dist);
+    }
+
+    // Draw the min distance (distance field)
+    color += m_dist;
 
     gl_FragColor = vec4(vec3(color), 1.0);
 }
