181ecfd66d8912e3f844909b517a02713406e67b
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-26 00:11:30 +0100

    Update timings

--- src/containers/Shaders/43/fragment.glsl
+++ src/containers/Shaders/43/fragment.glsl
@@ -32,8 +32,8 @@ float explosion (vec2 st, float t){
     for(float i = 0.0 ; i< NUM_PARTICLES; i++) {
         vec2 dir = Hash12_Polar(i + 1.0) * 0.5; 
         float d = length(st - dir * t);
-        float brightness = mix(0.0, 0.0005, smoothstep(0.0, 0.2, t));
-        brightness *= sin(t * 20. + i) * 0.5 + 0.5;
+        float brightness = mix(0.0, 0.0005, smoothstep(0.0, 0.1, t));
+        brightness *= sin(t * 15. + i) * 0.5 + 0.5; // blinking of firework while alive
         brightness *= smoothstep(1.0, 0.5, t); // fades out the effect before it restarts
         sparks += brightness / d ;
     }
@@ -52,8 +52,8 @@ void main()
         float ft = floor(t);
         vec3 colorValue = sin(vec3(.34 * part, .54 * (1.0 - part), 0.45) * ft) * 0.45 + 0.55;
         vec2 offset = Hash12(i + 1.0 + ft) - 0.5;
-        offset *= vec2(0.9, 0.25); //Shrink the possible explosion area
-        offset += vec2(0.0, 0.2); //Move the possible explosion area up
+        offset *= vec2(0.85, 0.15); //Shrink the possible explosion area
+        offset += vec2(0.0, 0.25); //Move the possible explosion area up
         color += explosion(st - offset, fract(t)) * colorValue;
     }
 
