d117d027fb605a9a1d93b77f576ffbe303a9051c
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-01-29 15:45:55 +0100

    Add smiley animated shader

--- src/containers/Shaders/46/fragment.glsl
+++ src/containers/Shaders/46/fragment.glsl
@@ -23,25 +23,63 @@ vec2 within (vec2 uv, vec4 rect){
     return (uv-rect.xy) / (rect.zw - rect.xy);
 }
 
-vec4 Eye(vec2 uv){
+vec4 Eye(vec2 uv, float side, vec2 m){
     uv -= 0.5;
     float d = length(uv);
+    uv.x *= side;
 
     vec4 irisCol = vec4(.3, .5,1.,1.);
     vec4 col = mix(vec4(1.), irisCol, S(.1 , .7, d) * 0.5);
+    col.a = S(0.5, .48, d);
 
-
-    col.rgb *= 1. - S(.45, .5, d) * 0.5 * sat(-uv.y - uv.x);
+    col.rgb *= 1. - S(.45, .5, d) * 0.5 * sat(-uv.y - uv.x * side);
+    d = length(uv - m * 0.2);
     col.rgb = mix(col.rgb, vec3(0.), S(.3, .28, d)); //iris outline
+
+
     irisCol.rgb *= 1. + S(.3, .05, d); 
     col.rgb = mix(col.rgb, irisCol.rgb, S(.28, .25, d));
+      d = length(uv - m * 0.25);
     col.rgb = mix(col.rgb, vec3(0.), S(.15, .14, d));
 
     float highlight = S(.1 , .09, length(uv - vec2(-0.15, .15)));
     highlight += S(.07 , .05, length(uv + vec2(-0.08, .08)));
     col.rgb = mix( col.rgb, vec3(1.), highlight);
 
-    col.a = S(0.5, .48, d);
+    
+    return col;
+}
+
+vec4 Brow(vec2 uv){
+    float y = uv.y;
+    uv.y += uv.x * .8 - .3;
+    uv.x -= .1;
+    uv -= .5;
+    vec4 col = vec4(0.);
+    float blur = .1;
+
+    float d1 = length(uv);
+    float s1 = S(.45, .45 - blur, d1);
+    float d2 = length(uv-vec2(.1, -.2) * .7);
+    float s2 = S(.5, .5 - blur, d2);
+    float browMask = sat(s1-s2);
+
+    float colMask = remap01(.7, .8, y) * .75;
+    colMask *= S(.6, .9, browMask);
+    vec4 browCol = mix(vec4(.4, .2, .2, 1.), vec4(1., .75, .5, 1.), colMask); 
+
+    uv.y += .15;
+    blur += .1;
+    d1 = length(uv);
+    s1 = S(.45, .45 - blur, d1);
+    d2 = length(uv-vec2(.1, -.2) * .7);
+    s2 = S(.5, .5 - blur, d2);
+
+    float shadowMask = sat(s1-s2)
+    col = mix(col, vec4(0.,0.,0.,1.), S(.0, .7, shadowMask)*0.6);
+    
+    col = mix(col, browCol, S(.2, .4, browMask));
+
     return col;
 }
 
@@ -83,9 +121,10 @@ vec4 Head(vec2 uv){
 
     float highlight = S(.41 , .405, d);
     highlight *= remap(.41, -.1, 0.75, 0., uv.y);
+    highlight *= S(.18, .19, length(uv - vec2(.21, .08)));
     col.rgb = mix(col.rgb, vec3(1.), highlight);
 
-    
+
 
     d = length( uv - vec2(.25,-0.2));
     float cheek = S(.2, .01, d) * 0.4;
@@ -96,17 +135,27 @@ vec4 Head(vec2 uv){
 }
 
 vec4 Smiley(vec2 uv){
+    vec2 m;
+    m.x = (uMouse.x - (uCanvasRes.x - uPlaneRes.x) * 0.5) / uPlaneRes.x;
+    m.y = ((uCanvasRes.y -  uMouse.y) - (uCanvasRes.y - uPlaneRes.y) * 0.5) / uPlaneRes.y;
+    m-=0.5;
+
     vec4 col = vec4(0.0);
+
+    float side = sign(uv.x);
     uv.x = abs(uv.x);
 
     vec4 head = Head(uv);
     col = mix(col, head, head.a);
 
-    vec4 eye= Eye(within(uv, vec4( .03, -.1, .37, .25)));
+    vec4 eye= Eye(within(uv, vec4( .03, -.1, .37, .25)), side, m);
     col = mix(col, eye, eye.a);
 
     vec4 mouth= Mouth(within(uv, vec4( -.3, -.4, .3, -.1)));
     col = mix(col, mouth, mouth.a);
+
+    vec4 brow= Brow(within(uv, vec4(.03, .2, .4, .45)));
+    col = mix(col, brow, brow.a);
 
     return col;
 }
